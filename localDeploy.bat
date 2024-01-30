1cc97350c5d868998c3a7fb92a0582344ac72528
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2024-01-30 14:39:47 +0100

    Fixes on sh/bat

--- localDeploy.bat
+++ localDeploy.bat
@@ -1,5 +1,44 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-docker-compose up --build -d
+set prefix=exam-manager-front-
 
-pause
+set react-app=%prefix%react-app
+
+set "services_not_found=0"
+set "services_processed=0"
+
+CALL :check_and_start_container %react-app%
+
+:check_and_start_container
+echo Container is %~1
+set "container_name=%~1"
+set /a services_processed+=1
+FOR /F "tokens=*" %%i IN ('docker ps -a -q --no-trunc --filter "name=!container_name!"') DO (
+    SET "container_id=%%i"
+    FOR /F "tokens=*" %%j IN ('docker ps -q --no-trunc --filter "id=%%i"') DO SET "running_container_id=%%j"
+    IF "!running_container_id!"=="" (
+        ECHO Starting !container_name!...
+        docker start %%i
+    ) ELSE (
+        ECHO !container_name! is already running.
+    )
+    goto :container_checked
+)
+set /a services_not_found+=1
+
+:container_checked
+goto :passed
+
+:passed
+IF !services_processed! GTR 1 (
+    IF !services_not_found! GTR 0 (
+        ECHO One or more required containers do not exist. Starting all services with Docker Compose...
+        docker-compose up -d
+    ) ELSE (
+        ECHO All required containers are up and running. No further action is taken.
+    )
+)
+
+:end
+endlocal
--- localDeploy.sh
+++ localDeploy.sh
@@ -1,3 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+prefix="exam-manager-front-"
+react_app_service="react-app"
 
-docker-compose up --build -d
+check_and_start_container() {
+  local container_name=$1
+  container_id=$(docker ps -a -q --no-trunc --filter "name=${prefix}${container_name}")
+
+  if [ -n "$container_id" ]; then
+    running=$(docker ps -q --no-trunc --filter "id=${container_id}")
+    if [ -z "$running" ]; then
+      echo "Starting $container_name..."
+      docker start "$container_id"
+    else
+      echo "$container_name is already running."
+    fi
+  else
+    echo "$container_name container does not exist."
+  fi
+}
+
+container_exists=$(docker ps -a --format '{{.Names}}' | grep -w "$react_app_service")
+
+if [ -n "$container_exists" ]; then
+  check_and_start_container "$react_app_service"
+else
+  echo "React app container ($react_app_service) does not exist. Building and starting all services..."
+  docker-compose up --build -d
+fi
